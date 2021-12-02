0f1184471e6e461dc72a532bb9ed995a054f5292
Jericho <desautelsj@hotmail.com>
2021-12-02 14:07:56 -0500

    Second attempt at fixing "error CS0246: The type or namespace name 'AssemblyFileVersion' could not be found" when building on AppVeyor

--- Source/Cake.Email.Common/Properties/AssemblyInfo.cs
+++ Source/Cake.Email.Common/Properties/AssemblyInfo.cs
@@ -1,13 +1,4 @@
 using System;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: InternalsVisibleTo("Cake.Email.Common.UnitTests")]
-[assembly: InternalsVisibleTo("Cake.Email.Common.IntegrationTests")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+using System.Reflection;
 
 [assembly: CLSCompliant(true)]
